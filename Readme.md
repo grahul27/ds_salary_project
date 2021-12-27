8df2fc94da0884ee6f2c04eec37e7f9cb2810bae
Rahul <31180528+guntharahul@users.noreply.github.com>
2021-12-26 21:47:37 -0700

    uploading data and data collection script

--- Readme.md
+++ Readme.md
@@ -2,7 +2,7 @@ Project Plan:
 
 1. Data Collection
    Either create your own data set or download dataset from Kaggle or Google Data Sets
-    Used a seleium web scraper to collect the data from glassdoor
+   Used a seleium web scraper to collect the data from glassdoor
 2. Data Cleaning
 3. EDA
 4. Idea
--- getData.py
+++ getData.py
@@ -1,12 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Dec 26 19:55:19 2021
-
-@author: gunth
-"""
-
 import glassdoor_scraper as gs
 import pandas as pd
 path="D:/UOA Study & Projects/Data Science Projects/ds_salary_project/chromedriver"
 
 df=gs.get_jobs('data scientist',15,False,path,15)
+df.to_csv('glassdoor_jobs.csv',index=False)
